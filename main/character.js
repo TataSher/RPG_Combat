9187839564bb0552cc3a8a1bea2ef5f7f903f131
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2021-07-27 14:45:56 +0100

    set character status to alive by default

--- main/character.js
+++ main/character.js
@@ -2,11 +2,12 @@ class Character {
   constructor() {
     this.health = 1000;
     this.level = 1;
+    this.alive = true;
   }
 }
 
 const char = new Character();
 
-console.log(char.level)
+console.log(char.alive)
 
 module.exports = Character;
--- test/character.test.js
+++ test/character.test.js
@@ -2,11 +2,16 @@ const { TestWatcher } = require('jest');
 const Character = require('../main/character');
 
 test('health to have 1000', () => {
-  character = new Character()
+  character = new Character();
   expect(character.health).toBe(1000);
 });
 
 test('set level to 1 by default', () => {
-  character = new Character()
+  character = new Character();
   expect(character.level).toBe(1);
-})
+});
+
+test('set character status to Alive by default', () => {
+  character = new Character();
+  expect(character.alive).toBe(true);
+});
