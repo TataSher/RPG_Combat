705ead7a9f77f46e8a2c1b6365a19bfc5b798fab
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2021-07-27 14:39:30 +0100

    Added health test & character constructor

---
+++ main/character.js
@@ -0,0 +1,11 @@
+class Character {
+  constructor() {
+    this.health = 1000;
+  }
+}
+
+// const char = new Character();
+
+// console.log(char.health)
+
+module.exports = Character;
--- test/character.test.js
+++ test/character.test.js
@@ -1,1 +1,7 @@
+const { TestWatcher } = require('jest');
 const Character = require('../main/character');
+
+test('health to have 1000', () => {
+  character = new Character()
+  expect(character.health).toBe(1000);
+});
