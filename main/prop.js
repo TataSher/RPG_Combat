a908d9a4c1fbbfa5428cb8d3b42f1cd62f2ce23c
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-12 15:04:52 +0100

    Created isDestroyed function

--- main/prop.js
+++ main/prop.js
@@ -2,11 +2,21 @@ class Prop {
   constructor() {
     this.health = 100;
     this.position = 0;
+    this.destroyed = false
   }
 
   checkHealth = () => this.health
 
   checkPosition = () => this.position
+
+  isDestroyed() {
+    console.log(this.health)
+    if (this.health <= 0) {
+      
+    this.destroyed = true
+    }
+    return this.destroyed
+  }
 };
 
 module.exports = Prop;
--- test/prop.test.js
+++ test/prop.test.js
@@ -1,9 +1,11 @@
 const { TestWatcher } = require('jest');
 const Prop = require('../main/prop');
 const Character = require('../main/character');
+// jest.mock('../main/character');
 
 
 beforeEach(() => {
+  // Character.mockClear();
   character1 = new Character
   prop1 = new Prop
 });
@@ -12,5 +14,21 @@ beforeEach(() => {
 test('props have a position and health', () => {
 
   expect(prop1.checkHealth()).toBe(100);
-  expect(prop1.checkPosition()).toBe(0)
+  expect(prop1.checkPosition()).toBe(0);
 });
+
+test('When reduced to 0 Health, prop is destroyed', () => {
+  // character1.attack = jest.fn()
+
+  times (5) (() => character1.attack(prop1));
+
+  expect(prop1.isDestroyed()).toBe(true);
+
+});
+
+const times = x => f => {
+  if (x > 0) {
+    f()
+    times (x - 1) (f)
+  }
+}
